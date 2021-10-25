1ea4b9b9c4e6d23b7a3e31e9fee1820b0b4f6c89
juan <soraioniosif@gmail.com>
2021-10-25 18:37:01 +0200

    ex 7 finished

--- src/ex7.js
+++ src/ex7.js
@@ -2,7 +2,7 @@
 
 const array = ['map', 'filter', 'find', 'some', 'index', 'reduce' ];
 
-//array.map( => console.log('value: ' +  + ', has index: ' + ));
+array.map( (a,index)=> console.log('value: ' +  + ', has index: ' + index));
 
 /* 
 output: 
