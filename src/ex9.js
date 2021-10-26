f46e71a4749653fd9695cbf663b853cf2a3dabcc
francisco <fjgraphaic@gmail.com>
2021-10-26 23:38:54 +0200

    listo creo el 9

--- src/ex9.js
+++ src/ex9.js
@@ -3,7 +3,7 @@
 const array = [0,1,2,3,4,5,6,7,8,9,10];
 
 const result = []; 
-//array. ((num, index) => determinar posicion donde queremos insertar el nuevo numero = num);
+array.map ((n , i) => result[(array.length -i)-1] = n);
 
 console.log(result);
 
