81ca2cfce32732f885717a0b4691534d209146ba
francisco <fjgraphaic@gmail.com>
2021-10-26 23:53:28 +0200

    listo el 10

--- src/ex10.js
+++ src/ex10.js
@@ -3,7 +3,7 @@
 const numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 
 
-const result = // numbers. (( ) => curr+acc);
+const result = numbers.reduce (( acc , curr) => curr+acc);
 
 console.log(result);
 
